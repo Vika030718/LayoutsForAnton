022a90044a72211bbe1e9f8c9a0a29794bd3b298
Viktoria Matveeva <vika030718@gmail.com>
2018-06-12 20:37:45 +0300

    some fixes

--- CalculatorV1.0/calculate_function.py
+++ CalculatorV1.0/calculate_function.py
@@ -1,3 +1,9 @@
+def calculator():
+    number1 = float(input('Please, enter your first number:'))
+    number2 = float(input('Please, enter your second number:'))
+    operation = input('Please, enter an operation(choose from: +, -, *, \\):')
+    print('Your result is: ' + str(calculate(number1, operation, number2)))
+
 def calculate(number1, operation, number2):
     if operation == '+':
         result = number1 + number2
@@ -5,7 +11,7 @@ def calculate(number1, operation, number2):
         result = number1 - number2
     elif operation == '*':
         result = number1 * number2
-    elif operation == '/':
+    elif operation == '\\':
         if number2 == 0:
             print('Nobody must divide by ziro!')
             return False
---
+++ CalculatorV1.0/calculator_v1.0.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+from calculate_function import calculate, calculator
+
+if __name__ == "__main__":
+    calculator()
+
